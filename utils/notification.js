046875c3211cd9c79718be22363b3542fc129f9a
Zohreh Karimi <zohrehkarimi019@gmail.com>
2024-10-03 19:10:04 +0330

    add retry logic for sending push notifications

--- utils/notification.js
+++ utils/notification.js
@@ -1,6 +1,38 @@
 const { Expo } = require('expo-server-sdk');
 const { getSecondsTillEndOfDay } = require('./date');
 
+const delayPromise = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const sendWithRetry = async (expo, chunk, retries = 5, delay = 1000) => {
+  for (let attempt = 1; attempt <= retries; attempt++) {
+    try {
+      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+      console.log('Success on attempt', attempt, ticketChunk);
+      return ticketChunk; // Success, return the result
+    } catch (error) {
+      console.error(`Error on attempt ${attempt}:`, error.message);
+      if (
+        error.code === 'ECONNRESET' ||
+        error.code === 'ETIMEDOUT' ||
+        error.message.includes('Client network socket disconnected')
+      ) {
+        if (attempt < retries) {
+          console.log(`Retrying after ${delay}ms...`);
+          await delayPromise(delay);
+          delay = delay * 2;
+        } else {
+          console.error('Max retries reached, notification failed.');
+        }
+      } else {
+        // If it's not a network error, break the retry loop and reject
+        console.error('Non-retriable error:', error.message);
+        break;
+      }
+    }
+  }
+  return []; // Return an empty array if the request ultimately fails
+};
+
 const sendHabitNotification = async (pushTokenItems, habit) => {
   const expo = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
   const ttl = Math.max(getSecondsTillEndOfDay(), 3600);
@@ -40,23 +72,20 @@ const sendHabitNotification = async (pushTokenItems, habit) => {
   let invalidTokens = new Set();
 
   for (let chunk of chunks) {
-    try {
-      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-      console.log(ticketChunk);
-      tickets.push(...ticketChunk);
+    const ticketChunk = await sendWithRetry(expo, chunk);
+    console.log(ticketChunk);
+    tickets.push(...ticketChunk);
 
-      // Check for errors in the ticketChunk
-      ticketChunk.forEach((ticket, index) => {
-        if (ticket.status === 'error') {
-          if (ticket.details?.error === 'DeviceNotRegistered') {
-            const token = chunk[index].to;
-            invalidTokens.add(token);
-          }
-        }
-      });
-    } catch (error) {
-      console.error('Error sending push notifications:\n', error);
-    }
+    // Check for errors in the ticketChunk
+    ticketChunk.forEach((ticket, index) => {
+      if (
+        ticket.status === 'error' &&
+        ticket.details?.error === 'DeviceNotRegistered'
+      ) {
+        const token = chunk[index].to;
+        invalidTokens.add(token);
+      }
+    });
   }
 
   return invalidTokens;
